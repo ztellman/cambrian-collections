dc703137ad30bbb692e7463999b4826e4ebcee84
Zach Tellman <ztellman@gmail.com>
2015-01-08 12:10:58 -0800

    target latest collection-check

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :source-paths ["generate"]
   :java-source-paths ["collections"]
   :dependencies
-  [[collection-check "0.1.4"]
+  [[collection-check "0.1.5-SNAPSHOT"]
    [criterium "0.4.3"]
    [org.clojure/clojure "1.6.0"]
    [org.eclipse.tycho/org.eclipse.jdt.core "3.10.0.v20140604-1726"]
